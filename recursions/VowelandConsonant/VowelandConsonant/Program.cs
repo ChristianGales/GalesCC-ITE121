1b1155dc3fb3d17494e338adc7d266dcb567828f
ChristianGales <99947337+ChristianGales@users.noreply.github.com>
2022-03-18 05:14:56 +0800

    Add files via upload

---
+++ recursions/VowelandConsonant/VowelandConsonant/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VowelandConsonant
+{
+    class Program
+    { 
+        string text = Console.ReadLine().ToLower();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[1] No of Vowels");
+            Console.WriteLine("[2] No of Consonants");
+            Console.Write("Select Function: ");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------------------------------------");
+
+            if (choice == 1)
+            {
+                vowel();
+            }
+            else if (choice == 2)
+            {
+                consonant();
+            }
+            else 
+            {
+                Console.WriteLine("Select from functions 1 and 2");
+            }
+
+            Console.WriteLine("--------------------------------------------------");
+            System.Threading.Thread.Sleep(1500);
+            Console.WriteLine("Created by: Gales, Christian Charles P. BSIT-1");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Instructor: Mr. Nesty Gordo");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+        private static void vowel()
+        {
+            var vowel = new[] { 'a', 'e', 'i', 'o', 'u' };
+            Console.Write("Enter a string: ");
+            var text = Console.ReadLine().ToLower();
+            var cvowel = text.Count(x => vowel.Contains(x));
+
+            Console.WriteLine("No of vowels : {0}", cvowel);
+    
+        }
+        private static void consonant()
+        {
+            var cons = new[] { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 
+            's', 't', 'v', 'w', 'x', 'y', 'z'};
+            Console.Write("Enter a string: ");
+            var text = Console.ReadLine().ToLower();
+            var ccons = text.Count(x => cons.Contains(x));
+
+            Console.WriteLine("No of consonants : {0}", ccons);
+        
+        }
+        
+    }
+}
---
+++ recursions/recursionconsonant/recursionconsonant/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class recursionconsonant
+{
+    static Boolean isConsonant(char ch)
+    {
+        ch = char.ToUpper(ch);
+        return (ch == 'A' || ch == 'E' ||
+                ch == 'I' || ch == 'O' ||
+                ch == 'U') == false &&
+                ch >= 65 && ch <= 90;
+    }
+    static int totalConsonants(String str, int n)
+    {
+        if (n == 1)
+        {
+            if (isConsonant(str[0]))
+                return 1;
+            else
+                return 0;
+        }
+        if (isConsonant(str[n - 1]))
+            return totalConsonants(str, n - 1) + 1;
+        else
+            return totalConsonants(str, n - 1);
+    }
+    public static void Main(String[] args)
+    {
+        Console.Write("Enter a String: ");
+        String str;
+        str = Console.ReadLine();
+        Console.WriteLine("Number of Consonants: " + totalConsonants(str, str.Length));
+
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------");
+        System.Threading.Thread.Sleep(1500);
+        Console.WriteLine("Created by: Gales, Christian Charles P. BSIT-1");
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine("Instructor: Mr. Nesty Gordo");
+        System.Threading.Thread.Sleep(1000);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+
+    
+}
+
